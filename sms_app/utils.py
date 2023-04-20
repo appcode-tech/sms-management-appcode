ae9f5bdef4ea367d4e74e142299fbdf9fcd71112
Ranit <ranit.das@appcodeindia.com>
2023-04-20 08:21:56 +0530

    + Msg91 txn sms (new) flow api

--- sms_app/utils.py
+++ sms_app/utils.py
@@ -33,7 +33,7 @@ class SMSMsg91:
         else:            
             url = "https://api.msg91.com/api/v5/otp?authkey=" + self.authKey  + "&mobile=" + self.mobile + "&template_id=" + template_id + "&unicode=" + unicode + "&otp_length=" + otp_length + "&otp_expiry=" + otp_expiry_in_mins + "&email=" + email
 
-            response = requests.request("POST", url, data=payload)
+            response = requests.post(url, data=payload)
             return response.text
 
     def ResendOTP(self):
@@ -44,7 +44,7 @@ class SMSMsg91:
         else:
             url = "https://api.msg91.com/api/v5/otp/retry?authkey=" + self.authKey  + "&mobile=" + self.mobile + "&retrytype=text"
 
-            response = requests.request("POST", url)
+            response = requests.post(url)
             return response.text
 
     def VerifyOTP(self, otp="", otp_expiry_in_mins = "1440"):
@@ -55,9 +55,10 @@ class SMSMsg91:
         else:
             url = "https://api.msg91.com/api/v5/otp/verify?otp=" + otp + "&authkey=" +  self.authKey  + "&mobile=" + self.mobile + "&otp_expiry=" + otp_expiry_in_mins
 
-            response = requests.request("POST", url)
+            response = requests.post(url)
             return response.text
 
+    # Old Txn SMS API using message params (Still functional)
     def SendTransactionSMS(self, message="", sender_id="", country="0", unicode="1", DLT_TE_ID=""):
         sandbox_response = '{"request_id":"000000000000000000000000","type":"success"}'
 
@@ -69,5 +70,38 @@ class SMSMsg91:
             headers = { 'authkey': self.authKey, 'content-type': "application/json" }
 
             payload = '{"sender":"'+ sender_id +'","route":"4","country":"'+ country +'","unicode":"'+ unicode +'","DLT_TE_ID": "'+ DLT_TE_ID +'","sms":[{"message":"' + message + '","to": ['+ str(self.mobile) +']}]}'
-            response = requests.request("POST", url, headers=headers, data=payload)
+            response = requests.post(url, headers=headers, data=payload)
+            return response.text
+    
+    ## New Txn SMS API using Template ID
+    def SendTransactionSMSFlow(self, template_id, sender, **vars):
+        """
+        Notes:
+            - sender: Sender ID is the approved entity sms header (eg. HDFCBK)
+            - vars: Additional variables key-value pairs for pass to template
+        """
+        sandbox_response = '{"request_id":"000000000000000000000000", "type":"success"}'
+
+        if self.sandbox_mode == True:
+            return sandbox_response
+        else:
+            url = "https://control.msg91.com/api/v5/flow/"
+
+            headers = {
+                'authkey': self.authKey,
+                'content-type': "application/json",
+                'accept': "application/json"
+            }
+
+            payload = {
+                "template_id": template_id,
+                "sender": sender,
+                "mobiles": str(self.mobile)
+            }
+
+            # Append any extra variables if required
+            for key, value in vars.items():
+                payload[key] = value
+            
+            response = requests.post(url, headers=headers, json=payload)
             return response.text
